ed75db08bfc43c4c95e85320934586e8ee9be821
Kato <kubkate@gmail.com>
2021-03-14 15:46:30 -0700

    display user detials in Card

--- src/components/Card.js
+++ src/components/Card.js
@@ -3,7 +3,36 @@ import { GithubContext } from '../context/context';
 import styled from 'styled-components';
 import { MdBusiness, MdLocationOn, MdLink } from 'react-icons/md';
 const Card = () => {
-  return <h2>card component</h2>;
+  const { githubUser }= React.useContext(GithubContext);
+  const {
+    avatar_url, 
+    html_url,
+    name,
+    company,
+    blog,
+    bio,
+    location,
+    twitter_username,  
+  } = githubUser;
+
+  return (
+    <Wrapper>
+      <header>
+        <img src={avatar_url} alt={name} />
+        <div>
+          <h4>{name}</h4>
+          <p>@{twitter_username || 'no_twitter'}</p>
+        </div>
+        <a href={html_url} target='_blank' rel='noreferrer'>follow</a>
+      </header>
+      <p className='bio'>{bio}</p>
+      <div className="links">
+        <p><MdBusiness/>{company || 'unknown'}</p>
+        <p><MdLocationOn/>{location || 'unknown'}</p>
+        {blog && <a href={`https://${blog}`} target='_blank' rel='noreferrer'><MdLink/>{company}</a>}
+      </div>
+    </Wrapper>
+    );
 };
 const Wrapper = styled.article`
   background: var(--clr-white);
@@ -13,7 +42,7 @@ const Wrapper = styled.article`
   border-bottom-right-radius: var(--radius);
   position: relative;
   &::before {
-    content: 'user';
+    content: 'user'; 
     position: absolute;
     top: 0;
     left: 0;
